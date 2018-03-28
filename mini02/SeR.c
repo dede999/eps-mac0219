fa9a8c0899e72bf28f1d0b0d750f6a1477a845c7
mauricio <mauriciolacardoso@yahoo.com.br>
2018-03-28 11:17:35 -0300

    mudança de nome de variáveis e correção do make

--- mini02/SeR.c
+++ mini02/SeR.c
@@ -6,12 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define LENGTH(x) (sizeof(x)/sizeof(x[0]))
+/*#define LENGTH(x) (sizeof(x)/sizeof(x[0]))*/
 
-typedef struct Animais {
-    int id; /* identifier */
+typedef struct pedra {
+    int id; /* identifier de animal ou pedra*/
     int n_pedra; /* # da casa (ou pedra) em que o animal está */
-} Animal;
+} Pedra;
 
 #define VAZIO 0 
 #define SAPO 1
@@ -22,15 +22,16 @@ typedef struct Animais {
  * * -1 rã
  * * 0 pedra vazia
  */
-Animal * lagoa;  /* pode ser um vetor de Animal ou inteiro */
+Pedra *lagoa;  /* pode ser um vetor de Animal ou inteiro */
 
 int pedras;
 int tam_lagoa;
 int cont = 0; /* representa a quantidade de vezes que um animal não se moveu */
 
 void movimentando (void * a) {
-    Animal spot = (Animal)a;
+    Pedra spot = (Pedra) a;
     printf("Animal %d na casa %d", spot.id, spot.n_pedra);
+    pthread_exit(NULL);
 }
 
 int main (int argc, char *argv[]) {
@@ -39,48 +40,51 @@ int main (int argc, char *argv[]) {
 	    printf("Formato de entrada: ./executavel numerosapos numeroras ");
         return 1;
     }
-    pthread_t *animais;
-    int sapos = atoi (argv[1]);
-    int ras = atoi (argv[2]);
-    int i, j = 0, k, ind, qtde_thread = 0; /* contadores */
+    else {
+        pthread_t *pedra;
+        int sapos = atoi (argv[1]);
+        int ras = atoi (argv[2]);
+        int i, j = 0, k, ind, qtde_thread = 0; /* contadores */
 
-    tam_lagoa = ras + sapos + 1;
+        tam_lagoa = ras + sapos + 1;
 
-    lagoa = malloc(tam_lagoa * sizeof(Animal));
-    animais = malloc((ras + sapos) * sizeof(pthread_t));
+        lagoa = malloc(tam_lagoa * sizeof(Pedra));
+        pedra = malloc((ras + sapos) * sizeof(pthread_t));
 
-    for (i = 0; i < ras + 1; ++i) {
-        lagoa[i].n_pedra = i;
-        if (i == ras) {
-            lagoa[i].id = VAZIO;
-        }
-        else {
+        for (i = 0; i < ras; ++i) { /*alocando rãs no vetor de pedras e pthreads*/
+            lagoa[i].n_pedra = i;        
             lagoa[i].id = RA;
-            if (pthread_create(animais[qtde_thread ++], lagoa[i], movimentando, NULL)) {
+            if (pthread_create(pedra[qtde_thread++], NULL, movimentando, (void *) lagoa[i])) {
                 printf("Erro ao criar uma thread RA\n");
                 exit(1);
             }
+            
         }
-    }
-    i = tam_lagoa -  1;
-    while (j < sapos) {
-        ind = i - j;
-        j ++;
-        lagoa[ind].id = SAPO;
-        lagoa[ind].n_pedra = ind;
-        if (pthread_create(&animais[qtde_thread ++], lagoa[ind], movimentando, NULL)) {
-            printf("Erro ao criar uma thread SAPO\n");
-            exit(1);
-        }
-    }
-    for (k = 0; k < (ras + sapos); ++k) {
-        if (pthread_join(animais[k], NULL)) {
-            printf("Erro ao finalizar uma das thread\n");
-            exit(1);
-        }
-    }
 
-    free(lagoa);
-    free(animais);
-    return 0;
+        lagoa[i].id = VAZIO;
+        lagoa[i].n_pedra = i;
+
+
+        i = tam_lagoa -  1;
+        while (j < sapos) {  /*alocando sapos no vetor de pedras e pthreads*/
+            ind = i - j;
+            j++;
+            lagoa[ind].id = SAPO;
+            lagoa[ind].n_pedra = ind;
+            if (pthread_create(pedra[qtde_thread++], NULL, movimentando, (void *) lagoa[ind])) {
+                printf("Erro ao criar uma thread SAPO\n");
+                exit(1);
+            }
+        }
+        for (k = 0; k < (ras + sapos); ++k) {
+            if (pthread_join(pedra[k], NULL)) {
+                printf("Erro ao finalizar uma das thread\n");
+                exit(1);
+            }
+        }
+
+        free(lagoa);
+        free(pedra);
+        return 0;
+    }
 }
