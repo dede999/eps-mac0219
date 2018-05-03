644b464cbe240097ba147a6cc96c42b26f3a930c
dede999 <andreluizsa00@gmail.com>
2018-05-03 18:15:07 -0300

    Mini EP4

---
+++ mini04/getdata.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import numpy as np
+
+print('sudo ./main %d %d000 > saida.txt' % 
+	(int(sys.argv[1]), int(sys.argv[2])))
+os.system('sudo ./main %d %s000 > saida.txt' % 
+	(int(sys.argv[1]), int(sys.argv[2])))
+
+entrada = open('saida.txt', 'r')
+bakery = [[], [], []]
+gate = [[], [], []]
+esse = [[], [], []]
+# alg = [bakery, gate]
+
+for line in entrada.readlines():
+	a = line.split(':')
+	if len(a) == 2:
+		if a[0].split(' ')[0] == 'Elapsed':
+			esse[0].append(int(a[1])/1000000)
+		if a[0].split(' ')[0] == 'Average':
+			esse[1].append(int(float(a[1])))
+		if a[0].split(' ')[0] == 'Standard':
+			esse[2].append(int(float(a[1])))
+	elif len(a) == 3:
+		alg = line.split(',')[0].split(':')[1].split(' ')[1]
+		if alg == 'bakery':
+			esse = bakery
+		else:
+			esse = gate
+
+print('bakery Elapsed %f' % np.array(bakery[0], float).mean())
+print('bakery Average %f' % np.array(bakery[1], float).mean())
+print('bakery Standard %f' % np.array(bakery[2], float).mean())
+print('gate Elapsed %f' % np.array(gate[0], float).mean())
+print('gate Average %f' % np.array(gate[1], float).mean())
+print('gate Standard %f' % np.array(gate[2], float).mean())
---
+++ mini04/statistics.h
@@ -0,0 +1,15 @@
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+void statistics_print_array(int n, const int v[]);
+
+double statistics_average(int n, const int v[]);
+
+/*Pass NULL to avg if you did not precalculate it*/
+double statistics_stddev(int n, const int v[], const double* avg);
+
+void statistics_print(int n, const int v[]);
+
+void statistics_avg_print(int n, const int v[]);
+
+#endif /*STATISTICS_H*/
