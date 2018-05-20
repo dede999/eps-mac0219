9ae953f517d8e62ebad7dec2b21f7dc699e9a865
mauricio <mauriciolacardoso@yahoo.com.br>
2018-05-20 13:56:07 -0300

    corrigido insercao no arquivo e adicionado algoritmo de multiplicao

--- EP1/pth.c
+++ EP1/pth.c
@@ -3,19 +3,21 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void le_matriz (double **matriz, int linhas, int colunas, FILE *arq){
+double** le_matriz (int linhas, int colunas, FILE *arq){
     int l, c;
     double valor;
-    matriz = malloc(linhas * sizeof(double *));
+    double **matriz = malloc(linhas * sizeof(double *));
 
     for (l = 0; l < linhas; ++l) {
         matriz[l] = calloc(colunas, sizeof(double));
     }
+
     while (fscanf(arq, "%d", &l) != EOF) {
         fscanf(arq, "%d", &c);
-        fscanf(arq, "%lf", &valor);
-        matriz[l][c] = valor;
+        fscanf(arq, "%lf", &valor);        
+        matriz[l-1][c-1] = valor;
     }
+    return matriz;
 }
 
 void imprime_matriz_arquivo (double **matriz, int linhas, int colunas, FILE *arq){
@@ -24,9 +26,32 @@ void imprime_matriz_arquivo (double **matriz, int linhas, int colunas, FILE *arq
     fprintf(arq, "%d %d\n", linhas, colunas);
     for (l = 0; l < linhas; ++l) {
         for (c = 0; c < colunas; ++c) {
-            if (valor != 0.0) fprintf("%d %d %lf\n", linhas, colunas, valor);
+            if (matriz[l][c] != 0.0) 
+                fprintf(arq, "%d %d %lf\n", linhas, colunas, matriz[l][c]);
         }
     }
+}
+
+double** multiplicaMatrizes(double **mat_a, double **mat_b, int la, int ca, int cb) {
+    int c, d, k;
+    double sum = 0;
+
+    double **mat_c = malloc (ca * sizeof(double *));
+    for (c = 0; c < la; c++)
+        mat_c[c] = malloc (cb * sizeof(double));
+
+
+    for (c = 0; c < la; c++) {
+      for (d = 0; d < cb; d++) {
+        for (k = 0; k < ca; k++) {
+          sum = sum + mat_a[c][k]*mat_b[k][d];
+        } 
+        mat_c[c][d] = sum;
+        sum = 0;
+      }
+    }
+
+    return mat_c;
 }
 
 void apaga (double **mat, int linhas){
@@ -51,7 +76,7 @@ int main (int argc, char **argv){
         return EXIT_FAILURE;
     }
 
-    if(fscanf(arqMatA, "%d", &la))
+    fscanf(arqMatA, "%d", &la);
     fscanf(arqMatA, "%d", &ca);
     fscanf(arqMatB, "%d", &lb);
     fscanf(arqMatB, "%d", &cb);
@@ -60,15 +85,17 @@ int main (int argc, char **argv){
         return EXIT_FAILURE;
     }
 
-    le_matriz(mat_a, la, ca, arqMatA);
-    le_matriz(mat_b, lb, cb, arqMatB);
+    mat_a = le_matriz(la, ca, arqMatA);
+    mat_b = le_matriz(lb, cb, arqMatB);
 
     /* multiplicação*/
+    mat_c = multiplicaMatrizes(mat_a, mat_b, la, ca, cb);
 
+    imprime_matriz_arquivo(mat_c, la, cb, arqMatC);
 
     /*imprime_matriz(mat_c, *la, *cb);*/
     apaga(mat_a, la);
     apaga(mat_b, lb);
-    /*apaga(mat_c, *la);*/
+    apaga(mat_c, la);
     return EXIT_SUCCESS;
 }
