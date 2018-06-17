bbdfea0f13b7438b6df4ac94b835ea0a1c987ade
dede999 <andreluizsa00@gmail.com>
2018-06-17 18:10:15 -0300

    inserindo código em CUDA

---
+++ EP1/andre.silveira.mauricio.cardoso/openmp.c
@@ -0,0 +1,105 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef _OPENMP
+#include <omp.h>
+#else
+#define omp_get_thread_num() 0
+#endif
+
+double** le_matriz (int linhas, int colunas, FILE *arq){
+    int l, c;
+    double valor;
+    double **matriz = malloc(linhas * sizeof(double *));
+
+    for (l = 0; l < linhas; ++l) {
+        matriz[l] = calloc(colunas, sizeof(double));
+    }
+
+    while (fscanf(arq, "%d", &l) != EOF) {
+        fscanf(arq, "%d", &c);
+        fscanf(arq, "%lf", &valor);        
+        matriz[l-1][c-1] = valor;
+    }
+    return matriz;
+}
+
+void imprime_matriz_arquivo (double **matriz, int linhas, int colunas, FILE *arq){
+    int l, c;
+    double valor = 0;
+    fprintf(arq, "%d %d\n", linhas, colunas);
+    for (l = 0; l < linhas; ++l) {
+        for (c = 0; c < colunas; ++c) {
+            if (matriz[l][c] != 0.0) 
+                fprintf(arq, "%d %d %.2lf\n", l + 1, c + 1, matriz[l][c]);
+        }
+    }
+}
+
+double** multiplicaMatrizes(double **mat_a, double **mat_b, int la, int ca, int cb) {
+    int c, d, k;
+    double sum = 0;
+
+    double **mat_c = malloc (ca * sizeof(double *));
+    for (c = 0; c < la; c++)
+        mat_c[c] = malloc (cb * sizeof(double));
+
+    #pragma omp parallel for schedule(static, 2) num_threads(4) ordered
+    for (c = 0; c < la; c++) {
+      for (d = 0; d < cb; d++) {
+        for (k = 0; k < ca; k++) {
+          sum = sum + mat_a[c][k]*mat_b[k][d];
+        } 
+        mat_c[c][d] = sum;
+        sum = 0;
+      }
+    }
+
+    return mat_c;
+}
+
+void apaga (double **mat, int linhas){
+    int i;
+    for (i = 0; i < linhas; ++i) {
+        free(mat[i]);
+    }
+    free(mat);
+}
+
+
+int main (int argc, char **argv){
+    FILE *arqMatA, *arqMatB, *arqMatC;
+    int la, ca, lb, cb;
+    double **mat_a, **mat_b, **mat_c;
+
+    arqMatA = fopen(argv[1], "r");
+    arqMatB = fopen(argv[2], "r");
+    arqMatC = fopen(argv[3], "w");
+    if (arqMatA == NULL || arqMatB == NULL || arqMatC == NULL){
+        printf("Deu ruim com algum arquivo\n");
+        return EXIT_FAILURE;
+    }
+
+    fscanf(arqMatA, "%d", &la);
+    fscanf(arqMatA, "%d", &ca);
+    fscanf(arqMatB, "%d", &lb);
+    fscanf(arqMatB, "%d", &cb);
+    if (ca != lb) { 
+        printf("Numeros de colunas de A e linhas de B devem ser iguais\n");
+        return EXIT_FAILURE;
+    }
+
+    mat_a = le_matriz(la, ca, arqMatA);
+    mat_b = le_matriz(lb, cb, arqMatB);
+
+    /* multiplicação*/
+    mat_c = multiplicaMatrizes(mat_a, mat_b, la, ca, cb);
+
+    imprime_matriz_arquivo(mat_c, la, cb, arqMatC);
+
+    /*imprime_matriz(mat_c, *la, *cb);*/
+    apaga(mat_a, la);
+    apaga(mat_b, lb);
+    apaga(mat_c, la);
+    return EXIT_SUCCESS;
+}
---
+++ EP2/ep.cu
@@ -0,0 +1,105 @@
+/** 
+* André Luiz Abdalla Silveira 8030353
+* Mauricio Luiz Cardoso 0000000
+* 
+* Esse programa escrito em CUDA visa criar um algoritmo que gera uma redução
+*	de matrizes. Cada matriz é representada por um vetor e todos estão 
+*	reunidos num vetor de vetores. A ideia é fazer uma função que faz uma 
+*	comparação entre vetores fazendo o mínimo de operações
+*
+*	Observação pro Mauricio (delete depois de ler): depois de passar um tempão
+*	pensando numa solução mais elegante, eu percebi que o número de comparações
+*	é o mesmo se compararmos com uma implemetação onde se contrapõem dois vetores
+*	iniciais, e o resultante da comparação com o próximo, e por aí vai.
+*	O que falta? 1- leitura de arquivos 2- fazer a parte de CUDA 3- corrigir eventuais
+*									bugs
+*/
+
+#include <stdio.h>
+#define E 9 # qtde de elementos de cada matriz
+
+/* pedaço de código copaido da apresentação, deve ser alterado sob demanda */
+__global__ void os_menores(int *a, int *b, int *c) {
+	c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+}
+
+void leitura (int *mat) {
+	int i, j;
+	for (i = 0; i < 3; i ++) {
+		printf("\n");
+		for (k = 0; k < 3; k ++) 
+			printf("%d\t", mat[3 * i + k]);
+		printf("\n");
+	}
+}
+
+int * compara (int *a, int *b) { /* Muitas coisas copiadas dos slides 36 e 37 */
+	int *resp, tam = E * sizeof(int);
+	int *d_a, *d_b, *d_resp;
+	
+	// Alloc space for device copies of a, b, c
+	cudaMalloc((void **) &d_a, tam);
+	cudaMalloc((void **) &d_b, tam);
+	cudaMalloc((void **) &d_resp, tam);
+
+	// Copy inputs to device
+	cudaMemcpy(d_a, a, tam, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b, tam, cudaMemcpyHostToDevice);
+
+	// Launch add() kernel on GPU with N threads -- mudar o valor de N
+	add<<<1,N>>>(d_a, d_b, d_resp);
+
+	// Copy result back to host
+	cudaMemcpy(resp, d_resp, tam, cudaMemcpyDeviceToHost);
+
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_resp);
+	return resp;
+}
+
+int * get_min (int **mat, int ini, int fim) {
+	int *parteA, *parteB;
+	if (ini - fim > 1){ /* mais que 2 elementos*/
+		meio = (ini + fim)/2;
+		parteA = get_min(mat, ini, meio);
+		parteB = get_min(mat, meio + 1, fim);
+		return compara(parteA, parteB);
+	}else if (ini - fim == 1){ /* 2 elementos */
+		return compara(mat[ini], mat[fim]);
+	}
+	else {
+		return mat[ini];
+	}
+}
+
+void apaga (int **mat){
+	int i;
+	int linhas = (sizeof(mat)/sizeof(int *));
+	for (i = 0; i < linhas; ++i) {
+		free(mat[i]);
+	}
+	free(mat);
+}
+
+int main () {
+	int qtde;
+	int **mat;
+	FILE *entrada;
+	int i; # contador
+
+	entrada =  fopen(argv[1], "r");
+	if (entrada == NULL) {
+		printf("Deu ruim pra abrir o arquivo");
+		return EXIT_FAILURE;
+	}
+	mat = (int *) malloc(qtde * sizeof(int *));
+	
+	for (i = 0; i < qtde; i ++){
+		mat[i] = (int) malloc(E * sizeof(int));
+	}
+
+	fclose(entrada);
+	leitura(get_min(mat, 0, qtde));
+	apaga(mat);
+}
