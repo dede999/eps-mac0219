6469a5a3bdd1b372936bd86c0851404be23f2716
mauricio <mauriciolacardoso@yahoo.com.br>
2018-06-19 08:08:47 -0300

    funcoes corrigidas e estrategias de alocacao de matrizes mudda

--- EP2/ep.cu
+++ EP2/ep.cu
@@ -17,19 +17,21 @@
 
 #include <stdio.h>
 #define E 9 // qtde de elementos de cada matriz
-/*#define linhaTam 3 */
+#define linhaElementos 3 // quantidade de elementos da linha 
+int numMatrizes;
 
 /* pedaço de código copaido da apresentação, deve ser alterado sob demanda */
 __global__ void os_menores(int *a, int *b, int *c) {
 	c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
 }
 
+/* Imprime todas as matrizes de dimensão ExE contidas en nat*/
 void leitura (int *mat) {
-	int i, j;
-	for (i = 0; i < 3; i ++) {
-		printf("\n");
-		for (k = 0; k < 3; k ++) 
-			printf("%d\t", mat[3 * i + k]);
+	int i, k;
+	for (i = 0; i < numMatrizes; i++) {
+		printf("********\n");
+		for (k = 0; k < linhaElementos; k++) 
+			printf("%d\t", *(mat++));
 		printf("\n");
 	}
 }
@@ -37,6 +39,7 @@ void leitura (int *mat) {
 int * compara (int *a, int *b) { /* Muitas coisas copiadas dos slides 36 e 37 */
 	int *resp, tam = E * sizeof(int);
 	int *d_a, *d_b, *d_resp;
+	int N = 32;
 	
 	// Alloc space for device copies of a, b, c
 	cudaMalloc((void **) &d_a, tam);
@@ -48,7 +51,7 @@ int * compara (int *a, int *b) { /* Muitas coisas copiadas dos slides 36 e 37 */
 	cudaMemcpy(d_b, b, tam, cudaMemcpyHostToDevice);
 
 	// Launch add() kernel on GPU with N threads -- mudar o valor de N
-	add<<<1,N>>>(d_a, d_b, d_resp);
+	os_menores<<<1,N>>>(d_a, d_b, d_resp);
 
 	// Copy result back to host
 	cudaMemcpy(resp, d_resp, tam, cudaMemcpyDeviceToHost);
@@ -59,54 +62,47 @@ int * compara (int *a, int *b) { /* Muitas coisas copiadas dos slides 36 e 37 */
 	return resp;
 }
 
-int * get_min (int **mat, int ini, int fim) {
-	int *parteA, *parteB;
-	if (ini - fim > 1){ /* mais que 2 elementos*/
-		meio = (ini + fim)/2;
-		parteA = get_min(mat, ini, meio);
-		parteB = get_min(mat, meio + 1, fim);
-		return compara(parteA, parteB);
-	}else if (ini - fim == 1){ /* 2 elementos */
-		return compara(mat[ini], mat[fim]);
-	}
-	else {
-		return mat[ini];
-	}
-}
+// int * get_min (int **mat, int ini, int fim) {
+// 	int *parteA, *parteB;
+// 	if (ini - fim > 1){ /* mais que 2 elementos*/
+// 		meio = (ini + fim)/2;
+// 		parteA = get_min(mat, ini, meio);
+// 		parteB = get_min(mat, meio + 1, fim);
+// 		return compara(parteA, parteB);
+// 	}else if (ini - fim == 1){ /* 2 elementos */
+// 		return compara(mat[ini], mat[fim]);
+// 	}
+// 	else {
+// 		return mat[ini];
+// 	}
+// }
 
-void apaga (int **mat){
-	int i;
-	int linhas = (sizeof(mat)/sizeof(int *));
-	for (i = 0; i < linhas; ++i) {
-		free(mat[i]);
-	}
-	free(mat);
-}
+// void apaga (int **mat){
+// 	int i;
+// 	int linhas = (sizeof(mat)/sizeof(int *));
+// 	for (i = 0; i < linhas; ++i) {
+// 		free(mat[i]);
+// 	}
+// 	free(mat);
+// }
 
-int** alocaMatrizes(int numMatrizes) {
-	int** mat = (int *) malloc(numMatrizes * sizeof(int *));
-	
-	for (int i = 0; i < numMatrizes; i ++){
-		mat[i] = (int) malloc(E * sizeof(int));
-	}
-	return mat;
-}
 
-double** alocaMatrizesArquivo(FILE *arq){
-    int numMatrizes, l, c;
+/* Le o arquivo arq que contem matrizes no formato declarado
+no enunciado e retorna um vetor com todas matrizes lidas*/
+int* alocaMatrizesArquivo(FILE *arq){
     char* asteriscos;
-    double valor;
-    double **matrizes;
+    int *matrizes, *matrizesAux;
 
     fscanf(arq, "%d", &numMatrizes);
-    matrizes = alocaMatrizes(numMatrizes);
+    matrizes = (int *) malloc(E * numMatrizes * sizeof(int));
     
+    matrizesAux = matrizes;
     for(int i = 0; i < numMatrizes; i++) {
-    	fscanf(arq, "%s", &asteriscos); //pula a linha de asteriscos
+    	fscanf(arq, "%s", asteriscos); //pula a linha de asteriscos
     	for(int j = 0; j < E; j++)
-        	fscanf(arq, "%d", &matrizes[i][j]);
+        	fscanf(arq, "%d", matrizesAux++);
     }
-    return matriz;
+    return matrizes;
 }
 
 
@@ -114,11 +110,8 @@ int main (int argc, char* argv[]) {
 	if(argc != 2) {
 		printf("Argumento do programa: nome do arquivo\n");
 	}
-	else {
-		int qtde;
-		int **mat;
+	else {	
 		FILE *entrada;
-		int i; # contador
 
 		entrada =  fopen(argv[1], "r");
 		if (entrada == NULL) {
@@ -126,9 +119,10 @@ int main (int argc, char* argv[]) {
 			return EXIT_FAILURE;
 		}
 
+		int *matrizes = alocaMatrizesArquivo(entrada);
 
 		fclose(entrada);
-		leitura(get_min(mat, 0, qtde));
-		apaga(mat);
+		// leitura(get_min(mat, 0, qtde));
+		free(matrizes);
 	}
 }
