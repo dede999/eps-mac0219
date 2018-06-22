91099a65d8ac48916862965d34dee1705eb1e71a
dede999 <andreluizsa00@gmail.com>
2018-06-21 23:31:06 -0300

    pequenas mudanças

--- EP2/ep.cu
+++ EP2/ep.cu
@@ -1,6 +1,6 @@
 /** 
 * André Luiz Abdalla Silveira 8030353
-* Mauricio Luiz Cardoso 0000000
+* Mauricio Luiz Cardoso 6796479
 * 
 * Esse programa escrito em CUDA visa criar um algoritmo que gera uma redução
 *   de matrizes. Cada matriz é representada por um vetor e todos estão 
@@ -21,48 +21,74 @@
 #define linhaElementos 3 // quantidade de elementos da linha 
 int numMatrizes;
 
-/* pedaço de código copaido da apresentação, deve ser alterado sob demanda */
-__global__ void os_menores(int *a, int *b, int *c) {
-    c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
+
+__global__ void os_menores(int *d_matrizes, int numMats, int jump) {
+  int indexAtual = indexIni = threadIdx.x + blockIdx.x * blockDim.x;
+  int tamMatriz = 9;
+
+  if(index < numMats * tamMatriz) {
+
+  }
+  else {
+    return;
+  }
+
 }
 
-/* Imprime todas as matrizes de dimensão ExE contidas en nat*/
-void leitura (int *mat) {
-    int i, k;
-    for (i = 0; i < numMatrizes * linhaElementos; i++) {
-        for (k = 0; k < linhaElementos; k++) 
-            printf("%d\t", *(mat++));
-        printf("\n");
-        if((i+1) % linhaElementos == 0)
-            printf("********************\n");
-    }
+/* Imprime todas as matrizes de dimensão ExE contidas em matrizes*/
+void leitura (int *matrizes) {
+  int i, k;
+  for (i = 0; i < numMatrizes * linhaElementos; i++) {
+    for (k = 0; k < linhaElementos; k++) 
+      printf("%d\t", *(matrizes++));
+    printf("\n");
+    if((i+1) % linhaElementos == 0)
+      printf("********************\n");
+  }
 }
 
-int * compara (int *a, int *b) { /* Muitas coisas copiadas dos slides 36 e 37 */
-    int *resp, tam = E * sizeof(int);
-    int *d_a, *d_b, *d_resp;
-    int N = 32;
-    
-    // Alloc space for device copies of a, b, c
-    cudaMalloc((void **) &d_a, tam);
-    cudaMalloc((void **) &d_b, tam);
-    cudaMalloc((void **) &d_resp, tam);
 
-    // Copy inputs to device
-    cudaMemcpy(d_a, a, tam, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b, tam, cudaMemcpyHostToDevice);
+void menorMatriz(int *d_matrizes, int numMats) {
+  int numBlocks = numMats / 30;
+  int numMatResto;
+  int numThreads = 27;
 
-    // Launch add() kernel on GPU with N threads -- mudar o valor de N
-    os_menores<<<1,N>>>(d_a, d_b, d_resp);
 
-    // Copy result back to host
-    cudaMemcpy(resp, d_resp, tam, cudaMemcpyDeviceToHost);
 
-    cudaFree(d_a);
-    cudaFree(d_b);
-    cudaFree(d_resp);
-    return resp;
+
+  os_menores<<<numBlocks, numThreads>>>(d_matrizes, numMats, numThreads * numBlocks);
+
+
+
+  // if((numMatResto = numMats % 30) != 0) {
+  //   // caso com chamada a mais
+
+  // }
+
+
+
 }
+
+
+void encontraMenorMatriz(int* matrizes) {
+  int tam = numMatrizes * E * sizeof(int);
+  int *d_matrizes;
+
+  // Alloc space for device copies of a, b, c
+  cudaMalloc((void **) &d_matrizes, tam);
+
+  // Copy inputs to device
+  cudaMemcpy(d_matrizes, matrizes, tam, cudaMemcpyHostToDevice);
+
+  // encontra menor matriz
+  menorMatriz(d_matrizes, numMatrizes);
+  
+  // Copy result back to host
+  cudaMemcpy(matrizes, d_matrizes, tam, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_matrizes);  
+}
+
 
 // int * get_min (int **mat, int ini, int fim) {
 //  int *parteA, *parteB;
@@ -92,38 +118,39 @@ int * compara (int *a, int *b) { /* Muitas coisas copiadas dos slides 36 e 37 */
 /* Le o arquivo arq que contem matrizes no formato declarado
 no enunciado e retorna um vetor com todas matrizes lidas*/
 int* alocaMatrizesArquivo(FILE *arq){
-    char asteriscos[10];
-    int *matrizes, *matrizesAux;
-    fscanf(arq, "%d", &numMatrizes);
-    matrizes =  (int *) malloc(E * numMatrizes * sizeof(int));
-    
-    matrizesAux = matrizes;
-    for(int i = 0; i < numMatrizes; i++) {
-        fscanf(arq, "%s", asteriscos); //pula a linha de asteriscos
-        for(int j = 0; j < E; j++)
-            fscanf(arq, "%d", matrizesAux++);
-    }
-    leitura(matrizes);
-    return matrizes;
+  char asteriscos[10];
+  int *matrizes, *matrizesAux;
+  fscanf(arq, "%d", &numMatrizes);
+  matrizes =  (int *) malloc(E * numMatrizes * sizeof(int));
+
+  matrizesAux = matrizes;
+  for(int i = 0; i < numMatrizes; i++) {
+    fscanf(arq, "%s", asteriscos); //pula a linha de asteriscos
+    for(int j = 0; j < E; j++)
+      fscanf(arq, "%d", matrizesAux++);
+  }
+  leitura(matrizes);
+  return matrizes;
 }
 
 
 int main (int argc, char* argv[]) {
-    if(argc != 2) {
-        printf("Argumento do programa: nome do arquivo\n");
+  if(argc != 2) {
+    printf("Argumento do programa: nome do arquivo\n");
+  }
+  else {  
+    FILE *entrada;
+    entrada =  fopen(argv[1], "r");
+    if (entrada == NULL) {
+      printf("Deu ruim pra abrir o arquivo\n");
+      return EXIT_FAILURE;
     }
-    else {  
-        FILE *entrada;
-        entrada =  fopen(argv[1], "r");
-        if (entrada == NULL) {
-            printf("Deu ruim pra abrir o arquivo\n");
-            return EXIT_FAILURE;
-        }
-        int *matrizes = alocaMatrizesArquivo(entrada);
+    int *matrizes = alocaMatrizesArquivo(entrada);
 
-        fclose(entrada);
-        // leitura(get_min(mat, 0, qtde));
-        free(matrizes);
-    }
+    fclose(entrada);
+    // leitura(get_min(mat, 0, qtde));
+    free(matrizes);
+    return EXIT_SUCCESS;
+  }
 }
 
